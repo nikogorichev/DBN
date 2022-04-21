883092c84df574e264aa3d12f3164ff7c75dc822
Anton <anton.yasinyuk@mail.ru>
2022-04-21 21:51:01 +0300

    stupor

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const config = require('./config/config');
 const mainRout = require('./routes/main.route');
 
 const app = express();
-
-const PORT = process.env.PORT ?? 3000;
+const PORT = process.env.PORT ?? 8080;
 
 config(app);
 
 app.use('/', mainRout);
-
 app.listen(PORT, () => console.log(`8===э Server started at ${PORT} port ****`));
--- routes/main.route.js
+++ routes/main.route.js
@@ -1,13 +1,64 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const { User } = require("../db/models");
 
-router.route('/')
+// главная страница
+router.route("/").get((req, res) => {
+  res.render("home", { home: false });
+});
+// login
+router
+  .route("/log")
   .get((req, res) => {
-    res.render('home');
+    res.render("partials/log");
+  })
+  .post(async (req, res) => {
+    const { password, mail } = req.body;
+    // проверочка подкатила на юзера в бд
+    try {
+      const user = await User.findOne({
+        where: {
+          mail,
+        },
+      });
+      if (!user) {
+        return res.render("partials/log", {
+          invalidMail: "ошибочка",
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
   });
 
+// регистрация
 
+router
+  .route("/registration")
+  .get((req, res) => {
+    res.render("partials/registration");
+  })
+  .post(async (req, res) => {
+    const { nickname, password, mail } = req.body;
 
+    // функция для проверки уникальности юзера
+    try {
+      const userUniq = await User.findOne({
+        where: {
+          mail,
+        },
+      });
+      if (userUniq) {
+        return res.render("registration", {
+          invalidForm: "систему не одурачить",
+        });
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
 
-
-  
+// ексит
+router.get("/logout", (req, res) => {
+  res.redirect("/");
+});
 module.exports = router;
