36e259a74138765bceeb68b616c22761ad3ae271
Nikolay <nikogorichev812@gmail.com>
2022-04-21 17:05:08 +0300

    release base

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const config = require('./config/config');
+const mainRout = require('./routes/main.route');
 
 const app = express();
 
 const PORT = process.env.PORT ?? 3000;
 
 config(app);
+
+app.use('/', mainRout);
 
 app.listen(PORT, () => console.log(`8===э Server started at ${PORT} port ****`));
