ca16ff7bf4d74b3b51cc2b0f34dff21707144c95
AlxKK <bobbytop@bk.ru>
2022-04-21 17:06:58 +0300

    release DB

---
+++ db/migrations/20220421134249-create-question.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Questions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id_theme: {
+        type: Sequelize.INTEGER
+      },
+      quest: {
+        type: Sequelize.INTEGER
+      },
+      answer: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Questions');
+  }
+};
