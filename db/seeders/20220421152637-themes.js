7da45ce74792f1dc152461a649c1dba394e2936b
AlxKK <bobbytop@bk.ru>
2022-04-21 21:13:25 +0300

    fDB

---
+++ db/seeders/20220421152637-themes.js
@@ -0,0 +1,42 @@
+'use strict';
+const fs = require('fs').promises;
+const path = require('path');
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+
+    let themeQ = await fs.readFile(path.join(process.env.PWD, 'themeQA','them.txt'),'utf-8');
+ 
+    let themeQA = themeQ.split('\n').filter((el) => el.length > 1)
+    
+    for(let i = 0; i < themeQA.length;i++){
+     
+        await queryInterface.bulkInsert('Themes', [{
+          title: themeQA[i],
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        }], {})
+      }
+    
+    
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
