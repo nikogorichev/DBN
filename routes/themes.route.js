c465652526811b89916611bfd6a2be1c2a810a3c
Nikolay <nikogorichev812@gmail.com>
2022-04-22 10:09:10 +0300

    routes hbss

--- routes/themes.route.js
+++ routes/themes.route.js
@@ -5,6 +5,7 @@ router.get('/:userId', async (req, res) => {
   const { userId } = req.params;
   const topics = await Theme.findAll({ raw: true });
   res.render('themes', { topics, userId });
+  
 });
 
 module.exports = router;
