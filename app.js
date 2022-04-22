97227a8bc41d27abc4b2b64656c4f43570ba635b
Nikolay <nikogorichev812@gmail.com>
2022-04-22 10:33:58 +0300

    remarks

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const themesRout = require('./routes/themes.route');
 const questRout = require('./routes/question.route');
 
 const app = express();
-const PORT = process.env.PORT ?? 8080;
+const PORT = process.env.PORT ?? 3000;
 
 config(app);
 
